a19a36f3f7374d9c2769748e54bd6e6854fd473d
yuny0623 <overman522@gmail.com>
2022-04-03 08:39:31 +0900

    day 2022-04-03

--- Leetcode 문제풀이/EASY/303_range_sum_query-immutable.py
+++ Leetcode 문제풀이/EASY/303_range_sum_query-immutable.py
@@ -1,5 +1,20 @@
 '''
-0834  ~ 
+0834  ~ 0839 
 '''
+class NumArray:
 
+    def __init__(self, nums: List[int]):
+        self.array = nums[:]
 
+    def sumRange(self, left: int, right: int) -> int:
+        if right > len(self.array) - 1:
+            return None
+        total = 0
+        for i in range(left, right + 1):
+            total += self.array[i]
+        return total 
+    
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
