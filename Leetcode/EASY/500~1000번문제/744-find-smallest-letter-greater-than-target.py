25e30d16e26319d43e11e8f1a09174e670fe355c
yuny0623 <overman522@gmail.com>
2022-05-15 14:33:39 +0900

    Add: day 2022-05-15

---
+++ Leetcode/EASY/500~1000번문제/744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,25 @@
+'''
+0225 ~ 0232
+Runtime: 119 ms, faster than 74.01% of Python3 online submissions for Find Smallest Letter Greater Than Target.
+Memory Usage: 14.2 MB, less than 91.13% of Python3 online submissions for Find Smallest Letter Greater Than Target.
+
+코멘트:
+쉬운 문제 
+'''
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        # 0226
+        letters = sorted(letters)
+        for i in letters:
+            if ord(i) > ord(target):
+                return i 
+        return letters[0]
+       
+    
+      
+     
+   
+  
+   
+
