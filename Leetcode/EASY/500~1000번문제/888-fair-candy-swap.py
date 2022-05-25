8b81deb1c3f043cf15fa27cba28474b7723488c1
yuny0623 <overman522@gmail.com>
2022-05-25 21:02:45 +0900

    Solve: 2022-05-25

--- Leetcode/EASY/500~1000번문제/888-fair-candy-swap.py
+++ Leetcode/EASY/500~1000번문제/888-fair-candy-swap.py
@@ -1,8 +1,16 @@
 '''
-
-
+0822 ~ <답지보고 풀음>
 
 코멘트: 
+처음에 접근법이 잘못됬음. Time Limit 뜬다. 
 '''
 
-
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        diff = abs(sum(aliceSizes) - sum(bobSizes)) // 2
+        print(diff)
+        for i in range(len(aliceSizes)):
+            for j in range(len(bobSizes)):
+                if abs(aliceSizes[i] - bobSizes[j]) == diff:
+                    return [aliceSizes[i], bobSizes[j]]
+        
