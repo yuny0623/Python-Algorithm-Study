e71bb8ba638b3bceebf6248a39ccc031df8e680f
yuny0623 <overman522@gmail.com>
2022-06-17 19:22:56 +0900

    Solve: 2022-06-17

--- Leetcode/EASY/1000~1500문제/1009-complement-of-base-10-integer.py
+++ Leetcode/EASY/1000~1500문제/1009-complement-of-base-10-integer.py
@@ -5,7 +5,7 @@ Memory Usage: 13.9 MB, less than 51.80% of Python3 online submissions for Comple
 
 코멘트: 
 문제 자체는 쉬움. 
-이것보다 쉽게 푸는방법이 분명 있을 것.
+이것보다 쉽게 푸는방법이 분명 있을 것. -> 더 줄일 수 있을듯 
 '''
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
@@ -17,3 +17,15 @@ class Solution:
                 result.append('1')
         return int(''.join(result), 2)
                 
+'''
+Runtime: 30 ms, faster than 92.01% of Python3 online submissions for Complement of Base 10 Integer.
+Memory Usage: 13.8 MB, less than 51.80% of Python3 online submissions for Complement of Base 10 Integer.
+
+두번째 솔루션: 
+이건 한줄로 끝낼 수 있다. 
+'''
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        return int(''.join(list(map(str, [1 - num for num in list(map(int, str(bin(n)[2:])))]))), 2)
+                
+
