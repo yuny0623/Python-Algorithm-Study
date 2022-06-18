598804ce836dc557a5e3c44923ec63c6eb3a3d4d
yuny0623 <overman522@gmail.com>
2022-06-18 19:58:59 +0900

    Solve: 2022-06-18

---
+++ Leetcode/EASY/1000~1500문제/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -0,0 +1,13 @@
+'''
+0741 ~ 0758 
+
+Runtime: 48 ms, faster than 69.93% of Python3 online submissions for Sum of Root To Leaf Binary Numbers.
+Memory Usage: 14.3 MB, less than 7.97% of Python3 online submissions for Sum of Root To Leaf Binary Numbers.
+
+코멘트: 
+왜 안풀릴까.
+
+'''
+
+
+
