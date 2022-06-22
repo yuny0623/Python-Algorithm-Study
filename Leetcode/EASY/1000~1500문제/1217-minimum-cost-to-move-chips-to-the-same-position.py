ee6053545031036f4ee7f8e9e05c983a6a3de6c2
yuny0623 <overman522@gmail.com>
2022-06-22 09:26:11 +0900

    Solve: 2022-06-22

--- Leetcode/EASY/1000~1500문제/1217-minimum-cost-to-move-chips-to-the-same-position.py
+++ Leetcode/EASY/1000~1500문제/1217-minimum-cost-to-move-chips-to-the-same-position.py
@@ -1,12 +1,26 @@
 '''
-0224 ~ 
+0224 ~ <답지보고 풀음 >
+
+Runtime: 66 ms, faster than 8.27% of Python3 online submissions for Split a String in Balanced Strings.
+Memory Usage: 13.8 MB, less than 55.94% of Python3 online submissions for Split a String in Balanced Strings.
+
+코멘트: 이건 내가 문제를 잘못 읽었다. 
+그리고 아래 코드처럼 count 변수를 가지고 비교하면서 ++, -- 해주는 동작은 앞으로 
+쓸일이 많을것같으니 이 방식은 기억해두자. 
 '''
-
-
-
-
-
-
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count = 0 
+        balancedCount = 0
+        for i in range(len(s)):
+            if s[i] == 'R':
+                count += 1
+            if s[i] == 'L':
+                count -= 1
+            if count == 0:
+                balancedCount += 1
+        
+        return balancedCount
 
 
 
---
+++ Leetcode/EASY/1000~1500문제/1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,71 @@
+'''
+0745 ~ <답지보고 풀음>
+답지: 
+https://www.youtube.com/watch?v=_tfiTQNZCbs
+'''
+
+'''
+오답 솔루션: 
+'''
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        coordinates = sorted(coordinates, key = lambda x: (x[0], x[1]))
+        print(coordinates)
+        count = 0
+        for i in range(1, len(coordinates)):
+            if coordinates[i-1][0] != coordinates[i][0]:
+                break
+            count += 1
+        if count == len(coordinates) - 1:
+            return True 
+        count = 0
+        for i in range(1, len(coordinates)):
+            if coordinates[i-1][1] != coordinates[i][1]:
+                break
+            count += 1
+        if count == len(coordinates) - 1:
+            return True 
+        # 기울기만 같은지 확인하면 될듯 
+        slope_x = coordinates[1][0] - coordinates[0][0] #
+        slope_y = coordinates[1][1] - coordinates[0][1] #
+        slope = 0
+        if slope_x == 0:
+            slope = 1
+        elif slope_y == 0:
+            slope = 1 
+        else:
+            slope = slope_y / slope_x
+        #slope = slope_y * slope_x
+        #print(slope)
+        if len(coordinates) == 2:
+            return True 
+        for i in range(2, len(coordinates)):
+            slope_x_i = coordinates[i][0] - coordinates[i - 1][0]
+            slope_y_i = coordinates[i][1] - coordinates[i - 1][1]
+            slope_i = 0
+            if slope_x_i == 0:
+                slope_i = 1
+            elif slope_y_i == 0:
+                slope_i = 1
+            else:
+                slope_i = slope_y_i / slope_x_i
+            #print(slope_i)
+            if slope != slope_i:
+                return False 
+        return True
+    
+
+
+'''
+정답 솔루션: 수학문제였음. 
+'''
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        for i in range(2, len(coordinates)):
+            if ((coordinates[1][1] - coordinates[0][1])*(coordinates[i][0] - coordinates[0][0]) 
+                != 
+                (coordinates[1][0] - coordinates[0][0])*(coordinates[i][1] - coordinates[0][1])):
+                return False 
+        return True 
+    
